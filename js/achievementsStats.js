980768c441f88c129d0c6893b05eeb761ca8464d
unknown <ffaannpl@gmail.com>
2020-01-23 20:48:11 +0100

    update renderowania elementu

---
+++ js/achievementsStats.js
@@ -0,0 +1,4 @@
+async function achievementStats(currentProxy){
+    res = await request(currentProxy + "https://api.achievementstats.com/games/1250/achievements/?key=")
+    //https://api.achievementstats.com/games/1250/achievements/?key=
+}
