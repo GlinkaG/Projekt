1d64f437cacbc98648aef6736b3c0c2dbea16406
unknown <ffaannpl@gmail.com>
2020-01-23 04:30:54 +0100

    aktualizacja zawierająca potencjalne dane do wykresów

---
+++ js/getGlobalAchievementPercentages.js
@@ -0,0 +1,6 @@
+async function getGlobalAchievementPercentages(currentProxy,appid){
+    //http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=440&format=xml
+    res = await request(currentProxy + "https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid="+appid)
+    //console.log(res)
+    return(res.achievementpercentages)
+}
