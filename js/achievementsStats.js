3fd4ac4425b1aab2fd3d0d79a15b2dcb9ec27d92
unknown <ffaannpl@gmail.com>
2020-01-26 15:05:46 +0100

    Pozbycie się nieużywanej funkcji achievementsStats, której funkcjonalność została zastąpiona poprzez funkcję getAchievements, czyszczenie console logów

--- js/achievementsStats.js
+++
@@ -1,4 +0,0 @@
-async function achievementStats(currentProxy){
-    res = await request(currentProxy + "https://api.achievementstats.com/games/1250/achievements/?key=")
-    //https://api.achievementstats.com/games/1250/achievements/?key=
-}
